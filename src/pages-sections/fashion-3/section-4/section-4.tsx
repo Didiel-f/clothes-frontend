72d8b311f259f1afbe6b39922b4ba56e6fb794aa
Didiel-f <didiel.f@gmail.com>
2025-07-28 22:47:33 -0400

    fix section 4

--- src/pages-sections/fashion-3/section-4/section-4.tsx
+++ src/pages-sections/fashion-3/section-4/section-4.tsx
@@ -4,11 +4,11 @@ import Container from "@mui/material/Container";
 import Heading from "../shared/heading";
 // GLOBAL CUSTOM COMPONENT
 import CategoryCard1 from "components/category-cards/category-card-1";
+import { getCategories } from "utils/__api__/fashion-2";
 // API FUNCTIONS
-import api from "utils/__api__/fashion-2";
 
 export default async function Section4() {
-  const categories = await api.getCategories();
+  const categories = await getCategories();
 
   return (
     <Container className="mt-4">
@@ -17,7 +17,7 @@ export default async function Section4() {
       <Grid container spacing={3}>
         {categories.map((item) => (
           <Grid size={{ md: 3, sm: 6, xs: 12 }} key={item.id}>
-            <CategoryCard1 image={item.image!} title={item.name} />
+            <CategoryCard1 image={item?.image?.url!} title={item.name} />
           </Grid>
         ))}
       </Grid>
