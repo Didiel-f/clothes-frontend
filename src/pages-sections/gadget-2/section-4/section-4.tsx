483f94a3a40809980e232d1609a17d27f65d5840
Didiel-f <didiel.f@gmail.com>
2025-08-03 20:14:48 -0400

    template cleaning

--- src/pages-sections/gadget-2/section-4/section-4.tsx
+++
@@ -1,9 +0,0 @@
-import Content from "./content";
-// API FUNCTIONS
-import api from "utils/__api__/gadget-2";
-
-export default async function Section4() {
-  const products = await api.getNewArrivalProducts();
-  if (!products || !products.length) return null;
-  return <Content products={products} />;
-}
