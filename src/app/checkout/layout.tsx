a10cd254b6795e185265ad5ef2b356091f5e336d
Didiel-f <didiel.f@gmail.com>
2025-10-07 20:24:24 -0300

    layout 2 deleted

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -1,23 +1,13 @@
 import { PropsWithChildren } from "react";
-import ShopLayout2 from "components/layouts/shop-layout-2";
-import { Navbar, NavigationList } from "components/navbar";
-// API FUNCTIONS
+import ShopLayout1 from "components/layouts/shop-layout-1";
 import api from "utils/__api__/layout";
 
 export default async function Layout({ children }: PropsWithChildren) {
   const data = await api.getLayoutData();
-
-  // NAVIGATION MENU LIST
-  const NAVIGATION = (
-    <Navbar
-      elevation={0}
-      navigation={<NavigationList navigation={data.header.navigation} />}
-    />
-  );
-
+  
   return (
-    <ShopLayout2 data={data} navbar={NAVIGATION}>
+    <ShopLayout1 data={data}>
       {children}
-    </ShopLayout2>
+    </ShopLayout1>
   );
 }
